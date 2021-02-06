416f150238502ec7562ce636c2ffa53cb27016a7
Vincent Leclerc <v.leclerc90@gmail.com>
2021-02-07 00:51:00 +0100

    scores tracking

--- index.js
+++ index.js
@@ -1,33 +1,39 @@
-const help = document.querySelector("#help");
-const helpModal = document.querySelector(".help-modal");
+const $help = document.querySelector("#help");
+const $helpModal = document.querySelector(".help-modal");
 
-const closeModal = document.querySelector(".close");
+const $closeModal = document.querySelector(".close");
 
-const playButton = document.querySelector(".start-btn");
-const reinitButton = document.querySelector(".reinit-btn");
+const $playButton = document.querySelector(".start-btn");
+const $reinitButton = document.querySelector(".reinit-btn");
 
-const gameButtons = document.querySelectorAll(".btn");
-const buttonColour = ["green", "red", "yellow", "blue"];
+const $gameButtons = document.querySelectorAll(".btn");
+const $buttonColour = ["green", "red", "yellow", "blue"];
+
+const $scoreLvl = document.querySelector("#lvl-score");
+const $scoreButtons = document.querySelector("#button-score");
 
 let gameColourPattern = [];
 let userColourPattern = [];
 
 let isSarted = false;
 let lvl = 0;
+let buttonsScore = 0;
+
+
+// scoreLvl.innerHTML = "Tours : " +  lvl;
+// scoreButtons.innerHTML = "Touches : " + buttonsScore;
 
 // Affiche la modal au clique de l'icone information
-help.addEventListener("click", () => {
-  helpModal.style.display = "flex";
+$help.addEventListener("click", () => {
+  $helpModal.style.display = "flex";
 });
 
 // Ferme la modal au clique de l'icone de fermeture
-closeModal.addEventListener("click", () => {
-  helpModal.style.display = "none";
+$closeModal.addEventListener("click", () => {
+  $helpModal.style.display = "none";
 });
 
-playButton.addEventListener("click", () => {
-  //   isSarted = true;
-  //   startGame();
+$playButton.addEventListener("click", () => {
 
   if (!isSarted) {
     isSarted = true;
@@ -39,11 +45,11 @@ playButton.addEventListener("click", () => {
   }
 });
 
-reinitButton.addEventListener("click", () => {
+$reinitButton.addEventListener("click", () => {
   reinitialize();
 });
 
-for (button of gameButtons) {
+for (button of $gameButtons) {
   button.addEventListener("click", (ev) => {
     if (isSarted) {
       let userChoice = ev.target.value;
@@ -62,12 +68,12 @@ for (button of gameButtons) {
 
 function newLevel() {
   let number = Math.floor(Math.random() * 4);
-  let colour = buttonColour[number];
+  let colour = $buttonColour[number];
 
   gameColourPattern.push(colour);
 
-  console.log("Level: " + lvl);
   console.log("Game colour pattern : " + gameColourPattern);
+  $scoreLvl.innerHTML = "Tours : " +  lvl;
   lvl++;
 }
 
@@ -79,6 +85,10 @@ function comparePatterns(i) {
   console.log("Current user pattern : " + currentUserColor);
 
   if (currentUserColor === currentGameColor) {
+    buttonsScore++;
+
+    $scoreButtons.innerHTML = "Touches : " + buttonsScore;
+
     if (gameColourPattern.length === userColourPattern.length) {
       
       userColourPattern = [];
@@ -95,18 +105,11 @@ function comparePatterns(i) {
 
 function reinitialize() {
   lvl = 0;
+  buttonsScore = 0;
   gameColourPattern = [];
   userColourPattern = [];
   isSarted = false;
 
-  console.log(
-    "Lvl : " +
-      lvl +
-      "\ngame pattern : " +
-      gameColourPattern +
-      "\nuser pattern : " +
-      userColourPattern +
-      "\nstarted : " +
-      isSarted
-  );
+  $scoreLvl.innerHTML = "Tours : " +  lvl;
+  $scoreButtons.innerHTML = "Touches : " + buttonsScore;
 }
