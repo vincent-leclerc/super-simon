c459b696185e7da12d6ff949417532489593fcc2
Vincent Leclerc <v.leclerc90@gmail.com>
2021-02-07 00:23:39 +0100

    compare patterns function

--- index.js
+++ index.js
@@ -3,53 +3,110 @@ const helpModal = document.querySelector(".help-modal");
 
 const closeModal = document.querySelector(".close");
 
-const playInput = document.querySelector(".start-btn");
-const gameButtons = document.querySelectorAll(".btn");
+const playButton = document.querySelector(".start-btn");
+const reinitButton = document.querySelector(".reinit-btn");
 
+const gameButtons = document.querySelectorAll(".btn");
 const buttonColour = ["green", "red", "yellow", "blue"];
-const gameColourPattern = [];
-const userColourPattern = [];
+
+let gameColourPattern = [];
+let userColourPattern = [];
 
 let isSarted = false;
 let lvl = 0;
 
 // Affiche la modal au clique de l'icone information
 help.addEventListener("click", () => {
-    helpModal.style.display = "flex";
+  helpModal.style.display = "flex";
 });
 
 // Ferme la modal au clique de l'icone de fermeture
 closeModal.addEventListener("click", () => {
-    helpModal.style.display = "none";
+  helpModal.style.display = "none";
 });
 
-playInput.addEventListener("click", () => {
+playButton.addEventListener("click", () => {
+  //   isSarted = true;
+  //   startGame();
+
+  if (!isSarted) {
     isSarted = true;
-    startGame();
-});
-
-function startGame() {
     console.log(isSarted);
 
-    for (button of gameButtons) {
-        button.addEventListener("click", (ev) => {
-            let userChoice = ev.target.value;
-            console.log(userChoice);
-    
-            userColourPattern.push(userChoice);
-    
-            console.log(userColourPattern);
-        });
+    newLevel();
+  } else {
+    console.log("Game already started");
+  }
+});
+
+reinitButton.addEventListener("click", () => {
+  reinitialize();
+});
+
+for (button of gameButtons) {
+  button.addEventListener("click", (ev) => {
+    if (isSarted) {
+      let userChoice = ev.target.value;
+      console.log("User choice : " + userChoice);
+
+      userColourPattern.push(userChoice);
+
+      const colorIndex = userColourPattern.length - 1;
+
+      comparePatterns(colorIndex);
+    } else {
+      console.log("Veuillez cliquer sur 'Lancer une partie' pour jouer");
     }
-    // newLevel();
-    // console.log(gameColourPattern);
+  });
 }
 
 function newLevel() {
-    let number = Math.floor(Math.random() * 4);
-    let colour = buttonColour[number];
+  let number = Math.floor(Math.random() * 4);
+  let colour = buttonColour[number];
 
-    gameColourPattern.push(colour);
+  gameColourPattern.push(colour);
 
-    lvl++;
+  console.log("Level: " + lvl);
+  console.log("Game colour pattern : " + gameColourPattern);
+  lvl++;
+}
+
+function comparePatterns(i) {
+  const currentGameColor = gameColourPattern[i];
+  const currentUserColor = userColourPattern[i];
+
+  console.log("Current game pattern : " + currentGameColor);
+  console.log("Current user pattern : " + currentUserColor);
+
+  if (currentUserColor === currentGameColor) {
+    if (gameColourPattern.length === userColourPattern.length) {
+      
+      userColourPattern = [];
+      console.log("Success !");
+      setTimeout(() => {
+        newLevel();
+      }, 600);
+    }
+  } else {
+    console.log("Game Over !");
+    reinitialize();
+  }
+}
+
+function reinitialize() {
+  lvl = 0;
+  gameColourPattern = [];
+  userColourPattern = [];
+  isSarted = false;
+
+  console.log(
+    "Lvl : " +
+      lvl +
+      "\ngame pattern : " +
+      gameColourPattern +
+      "\nuser pattern : " +
+      userColourPattern +
+      "\nstarted : " +
+      isSarted
+  );
 }
